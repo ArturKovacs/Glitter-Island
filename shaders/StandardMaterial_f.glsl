edd307573770d9d3109fdee67dba77bc12982471
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2015-08-31 22:50:42 +0200

    added ambient lighting to standard material

--- shaders/StandardMaterial_f.glsl
+++ shaders/StandardMaterial_f.glsl
@@ -38,12 +38,13 @@ void main(void)
 	
 	vec3 ks = texture(specularTexture, texCoord_v).xyz;
 	vec4 kd = texture(albedoTexture, texCoord_v) * (vec4(1) - vec4(ks, 0));
+	vec3 ambient = kd.rgb * 0.02;
 	
 	gl_FragDepth = mix(1.0, gl_FragCoord.z, kd.a);
 	
 	const float maxShininess = 256;
 	float shininess = (1 - texture(roughnessTexture, texCoord_v).x) * maxShininess;
 	
-	fragColor = vec4((kd.xyz + ks*PhongBlinn(lightDir, normalize(viewerDir_v), normal, shininess)) * max(dot(lightDir, normal), 0), kd.a); 
+	fragColor = vec4((kd.xyz + ks*PhongBlinn(lightDir, normalize(viewerDir_v), normal, shininess)) * max(dot(lightDir, normal), 0) + ambient, kd.a); 
 	//fragColor = vec4(normal, 1.0);
 } 
