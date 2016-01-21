2f9f5a7eb0af6a131c7b48abfdce29e1b703c48f
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2016-01-21 22:35:44 +0100

    Experimenting with water improvements

--- shaders/Water_f.glsl
+++ shaders/Water_f.glsl
@@ -30,13 +30,13 @@ vec2 D[waveCount] = vec2[waveCount](
 
 //wave frequencies
 float w[waveCount] = float[waveCount](
-	float(4.3), float(9.8), float(6.9), float(6.0),
-	float(7.2), float(4.7), float(9.9), float(5.9),
-	float(5.1), float(5.6), float(4.8), float(7.8),
-	float(6.0), float(10.5), float(8.7), float(5.7)
+	float(2.3), float(2.8), float(6.9), float(7.0),
+	float(3.2), float(4.7), float(3.9), float(5.9),
+	float(4.1), float(6.6), float(4.8), float(4.8),
+	float(4.0), float(2.5), float(4.7), float(5.7)
 );
 
-//wave frequencies*speeds packed in phi
+//wave (frequencies*speeds) packed in phi
 float phi[waveCount] = float[waveCount](
 	w[ 0]*0.31, w[ 1]*0.52, w[ 2]*0.52, w[ 3]*0.52,
 	w[ 4]*0.24, w[ 5]*0.34, w[ 6]*0.64, w[ 7]*0.64,
@@ -46,17 +46,17 @@ float phi[waveCount] = float[waveCount](
 
 //amplitudes
 float A[waveCount] = float[waveCount](
-	float(.005), float(.006), float(.004), float(.005),
-	float(.003), float(.006), float(.003), float(.005),
-	float(.006), float(.003), float(.006), float(.006),
-	float(.005), float(.004), float(.005), float(.006)
+	float(.009), float(.007), float(.004), float(.003),
+	float(.006), float(.006), float(.007), float(.004),
+	float(.004), float(.004), float(.006), float(.006),
+	float(.004), float(.007), float(.005), float(.004)
 );
 
 vec3 getWaveNormal(vec2 xy, float t)
 {
 	vec2 sum_d = vec2(0);
 	
-	const float k = 3;
+	const float k = 6;
 	for (int i = 0; i < waveCount; i++) {
 		//vec2 D_i = normalize(xy-D[i]*1000);
 		vec2 D_i = normalize(D[i]);
@@ -70,7 +70,7 @@ vec3 getWaveNormal(vec2 xy, float t)
 }
 
 const vec3 waterColor = vec3(0.8, 0.88, 0.95);
-const float shininess = 256;
+const float shininess = 512;
 const float n = 1.335;
 const float k = 0;
 const float F0 = ((n-1)*(n-1)+k*k)/((n+1)*(n+1)+k*k);
@@ -88,32 +88,34 @@ void main(void)
 	vec2 screenDimensions = vec2(screenWidth, screenHeight);
 	vec2 fragCoordTex = vec2(gl_FragCoord.xy/screenDimensions);
 	
-	vec4 screenSurfacePos = (invViewProj * vec4(
+	vec4 underwaterSurfacePos = (invViewProj * vec4(
 	                              (fragCoordTex.x*2-1),
 	                              (fragCoordTex.y*2-1),
 	                              (texelFetch(screenDepth, ivec2(gl_FragCoord.xy), 0).x)*2-1,
 	                              1));
 	
-	screenSurfacePos.xyz /= screenSurfacePos.w;
+	underwaterSurfacePos.xyz /= underwaterSurfacePos.w;
 	
-	float screenDistInWater = distance(screenSurfacePos.xyz, posFromVert);
-	float screenDistInWaterSqr;
-	{
-		vec3 posDiff = screenSurfacePos.xyz - posFromVert;
-		screenDistInWaterSqr = dot(posDiff, posDiff);
-	}
+	//float distanceInWater = distance(underwaterSurfacePos.xyz, posFromVert);
 	
 	vec3 fromCam = -viewDir;
 	vec3 refractedDir = refract(fromCam, normal, 1/n);
-	vec3 fakeHitPos = posFromVert+refractedDir*min(screenDistInWater, 0.25);
+	vec3 fakeHitPos = posFromVert+refractedDir*2;
 	vec4 ClipSpacePos = MVP * vec4(fakeHitPos, 1);
 	vec2 hitPosOnScreen = (ClipSpacePos.xy/ClipSpacePos.w) * 0.5 + vec2(0.5);
+	
+	vec2 txy = vec2(1)-max(abs(fragCoordTex-0.5)-0.37, 0)*10.0;
+	float refractionStrength = clamp(txy.x*txy.y, 0, 1);
+	hitPosOnScreen = mix(fragCoordTex, hitPosOnScreen, refractionStrength);
+	
 	float hitPosDepth = texelFetch(screenDepth, ivec2(hitPosOnScreen*screenDimensions), 0).x;
 	
 	vec3 refractedColor;
 	float refractedDepth;
-	if (hitPosDepth < gl_FragCoord.z) {
-		hitPosOnScreen = fragCoordTex;
+	
+	bool useFallbackSample = hitPosDepth <= gl_FragCoord.z;
+	if (useFallbackSample) {
+		//hitPosOnScreen = fragCoordTex;
 	}
 	
 	refractedColor = texture(screen, hitPosOnScreen).rgb;
@@ -121,14 +123,13 @@ void main(void)
 	
 	////////
 	vec4 refractedSurfacePos = (invViewProj * vec4(
-	                              (fragCoordTex.x*2-1),
-	                              (fragCoordTex.y*2-1),
+	                              (hitPosOnScreen.x*2-1),
+	                              (hitPosOnScreen.y*2-1),
 	                              (refractedDepth)*2-1,
 	                              1));
 	
 	refractedSurfacePos.xyz /= refractedSurfacePos.w;
 	
-	float distanceInWater = distance(refractedSurfacePos.xyz, posFromVert);
 	//TODO: this is not the actual water depth
 	float waterDepth = max(posFromVert.y - refractedSurfacePos.y, 0);
 	////////
@@ -144,7 +145,22 @@ void main(void)
 	refractedColor *= 1-fresnel;
 	reflectedColor *= fresnel;
 	
+	//TODO water still does not work properly
+	
+	float distanceInWater;// = distance(underwaterSurfacePos.xyz, posFromVert);
+	if (useFallbackSample) {
+		//distanceInWater = distance(underwaterSurfacePos.xyz, posFromVert);
+	}
+	else {
+		//distanceInWater = distance(refractedSurfacePos.xyz, posFromVert);
+	}
+	distanceInWater = distance(refractedSurfacePos.xyz, posFromVert);
+	if (useFallbackSample) {
+		distanceInWater = 0;
+	}
+	
+	//float distanceInWater = distance(underwaterSurfacePos.xyz, posFromVert);
 	//distanceInWater = mix(0, distanceInWater, -(exp(-waterDepth)-1));
-	distanceInWater = mix(0, distanceInWater, clamp(waterDepth, 0, 1)); 
-	fragColor = vec4(pow(waterColor, vec3(clamp(distanceInWater*0.4, 0, 25)))*refractedColor + reflectedColor + sunColor*specularIntensity, 1);
+	distanceInWater = distanceInWater*clamp(distanceInWater, 0, 1); 
+	fragColor = vec4(pow(waterColor, vec3(clamp(distanceInWater*0.6, 0, 25)))*refractedColor + reflectedColor + sunColor*specularIntensity, 1);
 }
